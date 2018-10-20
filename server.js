8e90339df24f7be019742285e3b6cf50ef0c37f8
bahybintang <bahybintang@gmail.com>
2018-10-20 17:02:28 +0700

    added info

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ function handleEvent(event) {
       });
   }
   else if(even.message.text === '/info'){
-      var data = "Hi I'm to do list bot XD XD XD\n/add <input> \n/end <index>\n/show\n/endall\n/yusficeo";
+      var data = "Hi I'm to do list bot XD XD XD\n/add <input> \n/end <index>\n/show\n/endall\n/yusficeo\n/info";
       return client.replyMessage(event.replyToken, {
         type: 'text',
         text: data
