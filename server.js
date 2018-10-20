a9e6adb7897362a992831509f596f51d045d06e4
bahybintang <bahybintang@gmail.com>
2018-10-20 12:27:16 +0700

    repair config

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const line = require('@line/bot-sdk');
 
-const config = require('./config');
+const config = require({
+    channelAccessToken: "MmtwD2FsTI8frwmTY8vqs14AoswdlmwgdUYruynm6U8vyK04ZuqImFEI8VNcl9H7uEoUHNyc3u0rZBK4VoAZiHse5Yv9e7LD8hymag+BEH8d4ZhAqCSQ2B+Uvth5K+5ZTq36dGZaBf+A2Muumk8+HgdB04t89/1O/w1cDnyilFU=",
+    channelSecret: "2129a67245bf02cf5eceacd98ef3b812"
+});
 
 const app = express();
 app.set('port', (process.env.PORT || 3000));
